53f31e3da723ddb29199fcd88a3047f142ac9d55
tonysmark <mjyuyu@qq.com>
2019-04-22 20:39:28 +0800

    bug终于改好了啊!!!!

--- src/components/BilibiliReview.vue
+++ src/components/BilibiliReview.vue
@@ -48,37 +48,6 @@ export default {
         console.log(this.index);
       }
     }
-  },
-  watch: {
-    bilisearch: function(bilisearch) {
-      async function biliSpider(key) {
-        const url =
-          "https://search.bilibili.com/all?keyword=" +
-          encodeURIComponent(key + "影评") +
-          "&from_source=banner_search";
-        const page = await request.get(url);
-        const $ = await cheerio.load(page.data);
-        let content = $(".video-contain li .title");
-        let map = [];
-        for (let i = 0; i < content.length; i++) {
-          let href = content[i].attribs["href"];
-          let reg = /\/av(\d*)/; // 正则匹配那去av号码
-          let [, avNum] = href.match(reg); // 结构赋值,只给数组中第二个参数
-          let iframeUrl = `https://player.bilibili.com/player.html?aid=${avNum}`;
-          map.push(iframeUrl);
-        }
-        return map;
-      }
-      let fetchData = async function(bilisearch) {
-        let res = biliSpider(bilisearch);
-        return res;
-      };
-      fetchData(bilisearch)
-        .then(e => {
-          this.urls = e;
-        })
-        .catch(e => console.log(e));
-    }
   }
 };
 </script>
--- src/components/imdbVideo.vue
+++ src/components/imdbVideo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="video">
-    <div class="video-container" v-if="videosrc">
-      <video id="tralier-video" :src="videosrc" controls></video>
+    <div class="video-container" v-if="getIMDBData">
+      <video id="tralier-video" :src="getIMDBData" controls></video>
     </div>
     <div class="wait-video" v-else>
       <Spin size="large" fix></Spin>
@@ -9,64 +9,22 @@
   </div>
 </template>
 <script>
-import { mapGetters } from "vuex";
-const requests = require("axios");
-const cheerio = require("cheerio");
+import { mapGetters, mapActions } from "vuex";
 export default {
   data() {
-    return {
-      videosrc: "",
-      imdbData: ""
-    };
+    return {};
   },
   computed: {
-    ...mapGetters(["getDoubanData"]) //召唤豆瓣
+    ...mapGetters(["getDoubanData", "getIMDBData"])
   },
-  created() {
-    this.imdbData = this.getDoubanData.imdb;
-    console.log(this.imdbData);
+  methods: {
+    ...mapActions(["onSearchImdb"])
   },
   watch: {
-    imdbData: function(imdbData) {
-      async function imdb(url) {
-        console.log("imdbData: " + imdbData);
-        let data = await mainpage(url);
-        let videoURL = await page2link(data);
-        return videoURL;
-      }
-      async function mainpage(url) {
-        const page = await requests(url);
-        const $ = cheerio.load(page.data);
-        const ttid = $(".slate_wrapper .slate a").attr("data-tconst");
-        const videoid = $(".slate_wrapper .slate a").attr("data-video");
-        console.log("imdb返回查询结果" + ttid + " " + videoid);
-        return { ttid, videoid };
-      }
-      async function page2link(data) {
-        let url2 = `https://www.imdb.com/_json/video/${data.ttid}`;
-        const page = await requests.get(url2);
-        const dataBody = page.data;
-        const current = await dataBody.videoMetadata[data.videoid];
-        let video = [];
-        let p1080 = current.encodings.filter(e => e.definition == "1080p");
-        let p720 = current.encodings.filter(e => e.definition == "720p");
-        let auto = current.encodings.filter(e => e.definition == "auto");
-        if (p1080 != "") {
-          video.push(p1080[0].videoUrl);
-        } else if (p720 != "") {
-          video.push(p720[0].videoUrl);
-        } else {
-          video.push(auto[0].videoUrl);
-        }
-        console.log("imdb预告视频：" + video[0]);
-        return video[0];
-      }
-      imdb(imdbData)
-        .then(e => {
-          console.log(e);
-          this.videosrc = e;
-        })
-        .catch(e => console.log(e));
+    getDoubanData: async function(val) {
+      let imdbLink = await val.imdb;
+      console.log("imdbLink: " + imdbLink);
+      this.onSearchImdb(imdbLink);
     }
   }
 };
