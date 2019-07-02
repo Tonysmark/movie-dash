1564861eff6771757f18a38fc2bae68570d680df
tonysmark <mjyuyu@qq.com>
2019-07-02 10:11:14 +0800

    新建文件 loacation

---
+++ src/views/location.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, doloribus quidem voluptatum pariatur error eaque! Voluptatum, ut voluptas! Doloribus et quae perferendis quis. Unde amet rem laboriosam pariatur! Accusantium, numquam!</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+#main {
+  background-color: #fff;
+}
+</style>
+
